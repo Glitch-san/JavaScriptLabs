995afab0bfdeda3ff3e5caab7c2c64ded7817c7b
Glitch-san <shichovaveronika@gmail.com>
2026-01-25 20:51:33 +1000

    Create №4.js

---
+++ КТ1/№4.js
@@ -0,0 +1,10 @@
+let world = prompt("Введите слово")
+let i = world.length
+if (Math.floor(i**0.5)**2 == i) {
+  let result = 1
+  console.log (result)
+}
+else {
+  let result = 0
+  console.log (result)
+}
